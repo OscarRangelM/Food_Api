d3a7db49449f3b1f0e65ad7570c0f0636b041202
OscarRangelM <orangelm1300@alumno.ipn.mx>
2023-04-19 16:02:55 -0600

    Create function dietsHandler

---
+++ api/src/handlers/dietsHandler.js
@@ -0,0 +1,12 @@
+const { getDietsController } = require('../controllers/dietsController.js');
+
+const getDiets = async (req, res) => {
+    try {
+        const getAllDiets = await getDietsController();
+        return res.status(200).send(getAllDiets);
+    } catch (error) {
+        res.status(404).json({ error: `Error getDiets ${error.message}` });
+    }
+}
+
+module.exports = { getDiets };
