752c9e80b72d8ad972e8ef98cecba4e1951de424
OscarRangelM <orangelm1300@alumno.ipn.mx>
2023-04-19 16:52:29 -0600

    recipe Handler

---
+++ api/src/handlers/recipeHandler.js
@@ -0,0 +1,41 @@
+const { getRecipeController, getIdRecipe, getRecipeName, createRecipe } = require('../controllers/recipeController.js');
+
+const getRecipe = async (req, res) => {
+    const { name } = req.query;
+    try {
+        if (name) {
+            const recipeByName = await getRecipeName(name);
+            return res.status(200).send(recipeByName);
+        }
+        const allRecipe = await getRecipeController();
+        return res.status(200).send(allRecipe);
+    } catch (error) {
+        res.status(404).json({ error: `Error getRecipe ${error.message}` });
+    }
+}
+
+const getRecipeById = async (req, res) => {
+    const { id } = req.params;
+    const source = isNaN(id) ? "db" : "api";
+
+    try {
+        const recipeId = await getIdRecipe(id, source);
+        res.status(200).send(recipeId);
+    } catch (error) {
+        res.status(404).json({ error: `Error getRecipeById ${error.message}` });
+    }
+
+}
+
+const postRecipe = async (req, res) => {
+    const { name, image, summary, healthScore, analyzedInstructions } = req.body;
+    try {
+        if (!name || !image || !summary || !healthScore || !analyzedInstructions) return res.status(401).json(`Data are needed for create this recipe`);
+        const newRecipe = await createRecipe({ name, image, summary, healthScore, analyzedInstructions });
+        res.status(200).send(newRecipe);
+    } catch (error) {
+        res.status(404).json({ error: `Error postRecipe ${error.message}` });
+    }
+}
+
+module.exports = { getRecipe, postRecipe, getRecipeById };
--- api/src/routes/recipe.js
+++ api/src/routes/recipe.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
-const { getRecipe, postRecipe } = require('../handlers/recipeHandler.js');
+const { getRecipe, postRecipe, getRecipeById } = require('../handlers/recipeHandler.js');
 
 const router = Router();
 
 router.get("/", getRecipe);
+router.get("/:id", getRecipeById);
 router.post("/", postRecipe);
 
 module.exports = router;
