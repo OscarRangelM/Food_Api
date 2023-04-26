2323bd6b4c28e11f40b2725d0ad647ef21ae55d3
OscarRangelM <orangelm1300@alumno.ipn.mx>
2023-04-26 17:22:43 -0600

    Agregamos todas las rutas de App

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,9 @@ import { Routes, Route } from 'react-router-dom';
 
 import Landing from './components/LandingPage/Landing.jsx';
 import Home from './components/Home/Home.jsx';
-
+import Detail from './components/DetailPage/Detail.jsx';
+import Form from './components/CreateRecipe/Form.jsx';
+import Favorites from './components/Favorites/Favorites.jsx';
 
 function App() {
   return (
@@ -13,9 +15,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} ></Route>
         <Route path="/home" element={<Home />} ></Route>
-        {/* <Route></Route> */}
-        {/* <Route></Route> */}
-        {/* <Route></Route> */}
+        <Route path="/detail/:detailId" element={<Detail />} ></Route>
+        <Route path="/createRecipe" element={<Form />} ></Route>
+        <Route path="/favorites" element={<Favorites />} ></Route>
       </Routes>
     </div>
   );
