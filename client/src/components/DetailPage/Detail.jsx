8bf74b717072644b6edfcb2430cc94a8344cbc05
OscarRangelM <orangelm1300@alumno.ipn.mx>
2023-05-10 14:15:20 -0600

    styles details

--- client/src/components/DetailPage/Detail.jsx
+++ client/src/components/DetailPage/Detail.jsx
@@ -3,33 +3,38 @@ import React, { useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { searchRecipeById } from '../../redux/actions/index.js';
-import Nav from '../Nav/Nav.jsx';
+import { NavLink } from 'react-router-dom';
 
 export default function Detail() {
 
     const dispatch = useDispatch();
-    const {detailId} = useParams();
+    const { detailId } = useParams();
     const state = useSelector(state => state.recipeDetail);
 
     const diets = state.diets;
 
     useEffect(() => {
         dispatch(searchRecipeById(detailId));
-    },[dispatch, detailId]);
+    }, [dispatch, detailId]);
 
     console.log(state);
 
     return (
         <div className={styles.divDetail}>
-             <Nav />
+            <div className={styles.sectionBack} >
+                <NavLink to='../home' >
+                    <button className={styles.bttnBack} >◀ Home</button>
+                </NavLink>
+            </div>
             <section className={styles.cardDetail} >
                 <img src={state.image} alt={state.name} className={styles.detailImg} />
                 <div className={styles.sectionInfoDetial} >
                     <h4 className={styles.detailName}>{state.name}</h4>
-                    <h5 className={styles.detailId}>{state.id}</h5>
-                    <h5 className={styles.detailSummary} >{state.summary}</h5>
-                    <h5 className={styles.detailHealthScore} >{state.healthScore}</h5>
-                    <h5 className={styles.detailInstructions} >{state.instructions}</h5>
+                    <p className={styles.detailId}>ID: {state.id}</p>
+                    <div dangerouslySetInnerHTML={{__html: state.summary} }></div>
+                    <p className={styles.detailHealthScore} >Health Score: {state.healthScore}</p>
+                    <p className={styles.detailInstructions} >{state.instructions}</p>
+                    <p className={styles.detailsIdTitle}>Diets:</p>
                     <ul className={styles.detailUl}>
                         {diets?.map(result => {
                             return (<li className={styles.detailListElement}>{result}</li>);
